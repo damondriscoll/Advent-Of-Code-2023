96de40c879296fab55e0eb084082f772279ac3f4
Enodma <damondriscoll@gmail.com>
2023-12-06 17:16:04 -0800

    day 1 part 2

--- day1/part1.py
+++ day1/part1.py
@@ -1,9 +1,17 @@
+# works for part1 and part2
+
 import re
+
+num_to_word = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 sum = 0
 with open('calibrations.txt', 'r') as f:
     for line in f:    
-        digits = re.findall('\d', line)
-        sum += int(f'{digits[0]}{digits[-1]}')
+        digits = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
+
+        first_digit = num_to_word.index(digits[0])+1 if digits[0] in num_to_word else digits[0]
+        last_digit = num_to_word.index(digits[-1])+1 if digits[-1] in num_to_word else digits[-1]
+
+        sum += int(f'{first_digit}{last_digit}')
 
 print(sum)
