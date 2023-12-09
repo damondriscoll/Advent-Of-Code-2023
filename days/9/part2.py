be08668375f2f14f9f7432a14a3dc7ee5086e03d
Enodma <damondriscoll@gmail.com>
2023-12-09 01:51:48 -0800

    day 9 part 2

---
+++ days/9/part2.py
@@ -0,0 +1,17 @@
+with open('inputs.txt', 'r') as f:
+    histories = f.read().split('\n')
+
+def getNextNumber(nums):
+    differences = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
+    if not all([v == 0 for v in differences]):
+        return differences[0] - getNextNumber(differences)
+    else:
+        return differences[0]
+
+sum = 0
+for history in histories:
+    history = list(map(int, history.split()))
+
+    sum += history[0] - getNextNumber(history)
+
+print(sum)
